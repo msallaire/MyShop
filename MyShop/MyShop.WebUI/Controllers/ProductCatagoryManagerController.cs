f8d7974c30f44c486e7336088ba69781659fb748
msallaire <smerialla@gmail.com>
2018-11-18 09:02:48 -0500

    added Roles&Claims

--- MyShop/MyShop.WebUI/Controllers/ProductCatagoryManagerController.cs
+++ MyShop/MyShop.WebUI/Controllers/ProductCatagoryManagerController.cs
@@ -9,6 +9,7 @@ using System.Web.Mvc;
 
 namespace MyShop.WebUI.Controllers
 {
+    [Authorize(Roles = "Admin")]
     public class ProductCatagoryManagerController : Controller
     {
         IRepository<ProductCatagory> context;
